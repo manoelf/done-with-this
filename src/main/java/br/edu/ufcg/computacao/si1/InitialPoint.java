6448ef0903e9bd3fa44705346f8d1e56e66950cd
manoelf <jose.ferreira@ccc.ufcg.edu.br>
2017-03-26 19:23:12 -0300

    Will work?

---
+++ src/main/java/br/edu/ufcg/computacao/si1/InitialPoint.java
@@ -0,0 +1,13 @@
+package br.edu.ufcg.computacao.si1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InitialPoint {
+
+    //Gerson branch
+	public static void main(String[] args) {
+		SpringApplication.run(InitialPoint.class, args);
+	}
+}
